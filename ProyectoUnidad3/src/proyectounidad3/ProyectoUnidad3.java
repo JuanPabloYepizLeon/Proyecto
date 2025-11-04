9a8d7a7070727059becdee3c15a36f4061961634
JuanPabloYepizLeon <juanpabloyepizleon@gmail.com>
2025-11-03 19:40:27 -0700

    Empezando

---
+++ ProyectoUnidad3/src/proyectounidad3/ProyectoUnidad3.java
@@ -0,0 +1,11 @@
+package proyectounidad3;
+
+public class ProyectoUnidad3 {
+
+    public static void main(String[] args) {
+        JFrameProyecto vtn = new JFrameProyecto();
+        vtn.setVisible(true);
+        vtn.setLocationRelativeTo(null);
+    }
+    
+}
